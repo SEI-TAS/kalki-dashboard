f4a15e3a4af29fcda0fcb6da0fccc30b35271299
ConnorMullaly <connor.f.mullaly@gmail.com>
2019-06-07 13:45:40 -0400

    able to add a device and view the table.  Table size decreased drastically to fit screen

--- dashboard/public/javascripts/dbManagement/device.js
+++ dashboard/public/javascripts/dbManagement/device.js
@@ -10,25 +10,29 @@ jQuery(document).ready(($) => {
 
     $.get("/devices", (devices) => {
         $.each(JSON.parse(devices), (index, device) => {
+            let deviceGroupName = device.group ? device.group.name : "N/A";
+            let lastAlertName = device.lastAlert ? device.lastAlert.name : "N/A";
+
             let newRow = "<tr id='tableRow" + device.id + "'>\n" +
                 "    <td class='fit'>" +
                 "        <div class='editDeleteContainer' >" +
-                "           <button type='button' class='btn btn-primary btn-sm' id='editButton" + device.id + "'>Edit</button>" +
-                "           <button type='button' class='btn btn-secondary btn-sm' id='deleteButton" + device.id + "'>Delete</button>" +
+                "           <button type='button' class='btn btn-primary btn-xs' id='editButton" + device.id + "'>Edit</button>" +
+                "           <button type='button' class='btn btn-secondary btn-xs' id='deleteButton" + device.id + "'>Delete</button>" +
                 "        </div>" +
                 "    </td>\n" +
                 "    <td class='fit' id='deviceID" + device.id + "'>" + device.id + "</td>\n" +
                 "    <td id='name" + device.id + "'>" + device.name + "</td>\n" +
                 "    <td id='description" + device.id + "'>" + device.description + "</td>\n" +
                 "    <td id='deviceType" + device.id + "'>" + device.type.name + "</td>\n" +
-                "    <td id='deviceGroup" + device.id + "'>" + device.group.name + "</td>\n" +
+                "    <td id='deviceGroup" + device.id + "'>" + deviceGroupName + "</td>\n" +
                 "    <td id='ipAddress" + device.id + "'>" + device.ip + "</td>\n" +
                 "    <td id='currentState" + device.id + "'>" + device.currentState.name + "</td>\n" +
-                "    <td id='lastAlert" + device.id + "'>" + device.lastAlert.name + "</td>\n" +
+                "    <td id='lastAlert" + device.id + "'>" + lastAlertName + "</td>\n" +
                 "    <td id='statusHistorySize" + device.id + "'>" + device.type.name + "</td>\n" +
                 "    <td id='samplingRate" + device.id + "'>" + device.samplingRate + "</td>\n" +
                 "    <td id='tags" + device.id + "'>" + device.tagIds + "</td>\n" +      //TODO: figure out how to get comma separated list from ids
                 "</tr>"
+
             deviceTable.row.add($(newRow)).draw();
 
             $("#deviceTableBody #editButton" + device.id).click(function () {
