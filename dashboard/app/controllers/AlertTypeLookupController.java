bf2dc65b7e4287823fafdbbd748cf300afddecfd
ConnorMullaly <connor.f.mullaly@gmail.com>
2019-07-30 15:53:57 -0400

    working checkpoint before needed changes are pushed to the kalki-db

---
+++ dashboard/app/controllers/AlertTypeLookupController.java
@@ -0,0 +1,89 @@
+package controllers;
+
+import edu.cmu.sei.ttg.kalki.models.*;
+import edu.cmu.sei.ttg.kalki.database.Postgres;
+
+import models.DatabaseExecutionContext;
+import play.libs.concurrent.HttpExecution;
+
+import play.mvc.Controller;
+import play.db.*;
+import play.mvc.Result;
+import play.data.*;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+
+import javax.inject.Inject;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeUnit;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public class AlertTypeLookupController extends Controller {
+    private final FormFactory formFactory;
+    private final DatabaseExecutionContext ec;
+    private final ObjectWriter ow;
+
+    @Inject
+    public AlertTypeLookupController(FormFactory formFactory, DatabaseExecutionContext ec) {
+        this.formFactory = formFactory;
+        this.ec = ec;
+        this.ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+    }
+
+    public CompletionStage<Result> getAlertTypeLookups() {
+        return CompletableFuture.supplyAsync(() -> {
+            List<AlertTypeLookup> alertTypeLookups = Postgres.findAllAlertTypeLookups();
+            try {
+                return ok(ow.writeValueAsString(alertTypeLookups));
+            } catch (JsonProcessingException e) {
+            }
+            return ok();
+        }, HttpExecution.fromThread((java.util.concurrent.Executor) ec));
+    }
+
+    public CompletionStage<Result> getAlertConditionsByDevice(int id) {
+        return CompletableFuture.supplyAsync(() -> {
+            List<AlertCondition> alertConditions = Postgres.findAlertConditionsByDevice(id);
+            try {
+                return ok(ow.writeValueAsString(alertConditions));
+            } catch (JsonProcessingException e) {
+            }
+            return ok();
+        }, HttpExecution.fromThread((java.util.concurrent.Executor) ec));
+    }
+
+    public CompletionStage<Result> addOrUpdateAlertTypeLookup() {
+        return CompletableFuture.supplyAsync(() -> {
+            Form<AlertTypeLookup> alertTypeLookupForm = formFactory.form(AlertTypeLookup.class);
+            Form<AlertTypeLookup> filledForm = alertTypeForm.bindFromRequest();
+
+            if (filledForm.hasErrors()) {
+                return badRequest(views.html.form.render(filledForm));
+            } else {
+                AlertTypeLookup atl = filledForm.get();
+
+                int n = atl.insertOrUpdate();
+                return redirect(routes.DBManagementController.dbManagementView(n));
+            }
+        }, HttpExecution.fromThread((java.util.concurrent.Executor) ec));
+    }
+
+    public CompletionStage<Result> deleteAlertTypeLookup() {
+        return CompletableFuture.supplyAsync(() -> {
+            String id = formFactory.form().bindFromRequest().get("id");
+            int idToInt;
+            try {
+                idToInt = Integer.parseInt(id);
+            } catch (NumberFormatException e) {
+                idToInt = -1;
+            }
+            Boolean isSuccess = Postgres.deleteAlertTypeLookup(idToInt);
+            return ok(isSuccess.toString());
+        }, HttpExecution.fromThread((java.util.concurrent.Executor) ec));
+    }
+}
