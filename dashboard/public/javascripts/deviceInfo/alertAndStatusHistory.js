6c6f0fe393027f515c166059a424ae1c5905baa0
cam292 <camazzotta@sei.cmu.edu>
2019-11-13 14:23:43 -0500

    Added more columns to tables on device info page

--- dashboard/public/javascripts/deviceInfo/alertAndStatusHistory.js
+++ dashboard/public/javascripts/deviceInfo/alertAndStatusHistory.js
@@ -24,11 +24,12 @@ jQuery(document).ready(($) => {
             let arr = JSON.parse(alertHistory);
             if(arr !== null && arr.length !== 0) {
                 arr.forEach(function(alert) {
-                    let newRow = "<tr id='tableRow" +alert.id+ "'>" +
-                        "   <td>" + moment(alert.timestamp).format(timeFormat) + "</td>" +
-                        "   <td>" + alert.name + "</td>" +
-                        "</tr>";
-                    alertHistoryTable.row.add($(newRow)).draw();
+                    appendToAlertTable(alert);
+                    // let newRow = "<tr id='tableRow" +alert.id+ "'>" +
+                    //     "   <td>" + moment(alert.timestamp).format(timeFormat) + "</td>" +
+                    //     "   <td>" + alert.name + "</td>" +
+                    //     "</tr>";
+                    // alertHistoryTable.row.add($(newRow)).draw();
                 });
             }
         });
@@ -40,27 +41,34 @@ jQuery(document).ready(($) => {
             if (newAlerts != null) {
                 newAlerts.forEach((alert) => {
                     if(alert.deviceId == given_id_alert) {
-                        let alertId = alert.id;
-                        let alertName = alert.name;
-                        let formattedTime = alert.timestamp ? moment(alert.timestamp).format(timeFormat) : "";
-
-                        let newRow = "<tr id='tableRow" +alertId+ "'>" +
-                            "   <td>" + formattedTime + "</td>" +
-                            "   <td>" + alertName + "</td>" +
-                            "</tr>";
-                        alertHistoryTable.row.add($(newRow)).draw();
-
-                        //add updated class to highlight updated row
-                        $("#alertHistoryTable #tableRow" + alertId).addClass("updated");
-
-                        //remove row highlight after three seconds
-                        setTimeout(function() {
-                            $("#alertHistoryTable #tableRow" + alertId).removeClass("updated");
-                        }, 3000);
+                        appendToAlertTable(alert);
                     }
                 });
             }
         });
+    }
+
+    // add id, device_status_id, alerter_id. possibly link alerter_id to umbox_instance page
+    function appendToAlertTable(alert) {
+        let formattedTime = alert.timestamp ? moment(alert.timestamp).format(timeFormat) : "";
+
+        let newRow = "<tr id='tableRow" +alert.id+ "'>" +
+            "   <td>" + alert.id + "</td>" +
+            "   <td>" + alert.name + "</td>" +
+            "   <td>" + alert.deviceStatusId + "</td>" +
+            "   <td>" + alert.alerterId + "</td>" +
+            "   <td>" + formattedTime + "</td>" +
+            "</tr>";
+
+        alertHistoryTable.row.add($(newRow)).draw();
+
+        //add updated class to highlight updated row
+        $("#alertHistoryTable #tableRow" + alert.id).addClass("updated");
+
+        //remove row highlight after three seconds
+        setTimeout(function() {
+            $("#alertHistoryTable #tableRow" + alert.id).removeClass("updated");
+        }, 3000);
     }
 
     /*
@@ -114,10 +122,10 @@ jQuery(document).ready(($) => {
                 lowestStatusId = statuses[ind].id;
             }
             statuses.forEach((status) => {
-               console.log(getUrlParameter('id')); 
 		if(getUrlParameter('id') == status.deviceId && !originalStatusIds.has(status.id)){
                     originalStatusIds.add(status.id);
                     let newRow = "<tr id='tableRow" + status.id + "'>" +
+                        "   <td>" + status.id + "</td>" +
                         "   <td>" + moment(status.timestamp).format(timeFormat) + "</td>" +
                         "   <td>" + makeAttributesString(status.attributes) + "</td>" +
                         "</tr>";
