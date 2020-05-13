23a403ac12df882e172c396c622398d417ba7efa
Keegan Williams <kmwilliams@sei.cmu.edu>
2020-05-12 20:20:59 -0400

    Added in some missing forms from the clearing forms before the page loads.

--- dashboard/public/javascripts/dbManagement/dbManagement.js
+++ dashboard/public/javascripts/dbManagement/dbManagement.js
@@ -27,9 +27,11 @@ $(window).on('load', function(){
     $.post("/clear-device-type-form", {}, function () {});
     $.post("/clear-group-form", {}, function () {});
     $.post("/clear-security-state-form", {}, function () {});
+    $.post("/clear-data-node-form", {}, function () {});
     $.post("/clear-tag-form", {}, function () {});
     $.post("/clear-umbox-image-form", {}, function () {});
     $.post("/clear-device-form", {}, function () {});
     $.post("/clear-command-lookup-form", {}, function () {});
     $.post("/clear-umbox-lookup-form", {}, function () {});
+    $.post("/clear-command-form", {}, function () {});
 });
