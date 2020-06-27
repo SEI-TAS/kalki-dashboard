42c7134d1e70d0aceb63152e26626f06b252b003
Keegan Williams <kmwilliams@sei.cmu.edu>
2020-06-27 12:45:32 -1000

    feat(controllers): add PolicyRuleController and PolicyConditionController
    Create Policy Rule and Policy Condition controllers for interacting with the database. Use common styles from other controllers, and implement usage of the updatingId field.

---
+++ dashboard/app/controllers/PolicyConditionController.java
@@ -0,0 +1,64 @@
+package controllers;
+
+import edu.cmu.sei.kalki.db.models.*;
+import edu.cmu.sei.kalki.db.daos.PolicyConditionDAO;
+
+import models.DatabaseExecutionContext;
+
+import play.libs.concurrent.HttpExecution;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.data.*;
+
+import javax.inject.Inject;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public class PolicyConditionController extends Controller {
+    private final FormFactory formFactory;
+    private final DatabaseExecutionContext ec;
+    private final ObjectWriter ow;
+    private int updatingId;
+
+    @Inject
+    public PolicyConditionController(FormFactory formFactory, DatabaseExecutionContext ec) {
+        this.formFactory = formFactory;
+        this.ec = ec;
+        this.ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        this.updatingId = -1; //if the value is -1, it means there should be a new policy condition
+    }
+
+    public CompletionStage<Result> getPolicyConditions() {
+        return CompletableFuture.supplyAsync(() -> {
+            List<PolicyCondition> policyConditions = PolicyConditionDAO.findAllPolicyConditions();
+            try {
+                return ok(ow.writeValueAsString(policyConditions));
+            } catch (JsonProcessingException ignored) {
+            }
+            return ok();
+        }, HttpExecution.fromThread((java.util.concurrent.Executor) ec));
+    }
+
+    public CompletionStage<Result> addOrUpdatePolicyCondition() {
+        return CompletableFuture.supplyAsync(() -> {
+            Form<PolicyCondition> policyRuleForm = formFactory.form(PolicyCondition.class);
+            Form<PolicyCondition> filledForm = policyRuleForm.bindFromRequest();
+
+            if (filledForm.hasErrors()) {
+                return badRequest(views.html.form.render(filledForm));
+            } else {
+                PolicyCondition pc = filledForm.get();
+                pc.setId(this.updatingId);
+                this.updatingId = -1;
+
+                int n = pc.insert();
+                return redirect(routes.DBManagementController.dbManagementDeviceTypeView(n));
+            }
+        }, HttpExecution.fromThread((java.util.concurrent.Executor) ec));
+    }
+}
---
+++ dashboard/app/controllers/PolicyRuleController.java
@@ -0,0 +1,131 @@
+package controllers;
+
+import edu.cmu.sei.kalki.db.daos.AlertTypeDAO;
+import edu.cmu.sei.kalki.db.models.*;
+import edu.cmu.sei.kalki.db.daos.PolicyRuleDAO;
+import edu.cmu.sei.kalki.db.daos.PolicyConditionDAO;
+
+import models.DatabaseExecutionContext;
+
+import play.libs.concurrent.HttpExecution;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.data.*;
+
+import javax.inject.Inject;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public class PolicyRuleController extends Controller {
+    private final FormFactory formFactory;
+    private final DatabaseExecutionContext ec;
+    private final ObjectWriter ow;
+    private int updatingId;
+
+    @Inject
+    public PolicyRuleController(FormFactory formFactory, DatabaseExecutionContext ec) {
+        this.formFactory = formFactory;
+        this.ec = ec;
+        this.ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        this.updatingId = -1; //if the value is -1, it means there should be a new policy
+    }
+
+    public CompletionStage<Result> getPolicyRule(int id) {
+        return CompletableFuture.supplyAsync(() -> {
+            PolicyRule policyRule = PolicyRuleDAO.findPolicyRule(id);
+            try {
+                return ok(ow.writeValueAsString(policyRule));
+            } catch (JsonProcessingException ignored) {
+            }
+            return ok();
+        }, HttpExecution.fromThread((java.util.concurrent.Executor) ec));
+    }
+
+    public CompletionStage<Result> getPolicyRules() {
+        return CompletableFuture.supplyAsync(() -> {
+            List<PolicyRule> policyRules = PolicyRuleDAO.findAllPolicyRules();
+            try {
+                return ok(ow.writeValueAsString(policyRules));
+            } catch (JsonProcessingException ignored) {
+            }
+            return ok();
+        }, HttpExecution.fromThread((java.util.concurrent.Executor) ec));
+    }
+
+    public CompletionStage<Result> addOrUpdatePolicyRule() {
+        return CompletableFuture.supplyAsync(() -> {
+            // We need to create a policy condition which will be used in the policy rule
+            Form<PolicyCondition> policyConditionForm = formFactory.form(PolicyCondition.class);
+            Form<PolicyCondition> policyConditionFilledForm = policyConditionForm.bindFromRequest();
+
+            if (policyConditionFilledForm.hasErrors()) {
+                return badRequest(views.html.form.render(policyConditionFilledForm));
+            } else {
+                // Form is good, so create the policy condition and insert it into the db
+                PolicyCondition pc = policyConditionFilledForm.get();
+
+                int policyConditionId = pc.insert();
+
+                // Policy condition created, so now lets make the policy rule
+                DynamicForm policyRuleForm = formFactory.form();
+                DynamicForm policyRuleFilledForm = policyRuleForm.bindFromRequest();
+
+                if (policyRuleFilledForm.hasErrors()) {
+                    // Remove the policy condition just made, because we cant convert it into a policy rule
+                    PolicyConditionDAO.deletePolicyCondition(policyConditionId);
+                    return badRequest(views.html.form.render(policyRuleFilledForm));
+                } else {
+                    // Form is good, so gather the components from the form, create the policy rule, and insert it
+                    int stateTransitionId = Integer.parseInt(policyRuleFilledForm.get("stateTransitionId"));
+                    int deviceTypeId = Integer.parseInt(policyRuleFilledForm.get("deviceTypeId"));
+                    int samplingRateFactor = Integer.parseInt(policyRuleFilledForm.get("samplingRateFactor"));
+
+                    PolicyRule pr = new PolicyRule(stateTransitionId, policyConditionId, deviceTypeId,
+                            samplingRateFactor);
+
+                    pr.setId(this.updatingId);
+                    this.updatingId = -1;
+
+                    int n = pr.insert();
+                    return redirect(routes.DBManagementController.dbManagementDeviceTypeView(n));
+                }
+            }
+        }, HttpExecution.fromThread((java.util.concurrent.Executor) ec));
+    }
+
+    public Result editPolicyRule() {
+        String id = formFactory.form().bindFromRequest().get("id");
+        int idToInt;
+        try {
+            idToInt = Integer.parseInt(id);
+        } catch (NumberFormatException e) {
+            idToInt = -1;
+        }
+        this.updatingId = idToInt;
+        return ok();
+    }
+
+    public CompletionStage<Result> deletePolicyRule() {
+        return CompletableFuture.supplyAsync(() -> {
+            String id = formFactory.form().bindFromRequest().get("id");
+            int idToInt;
+            try {
+                idToInt = Integer.parseInt(id);
+            } catch (NumberFormatException e) {
+                idToInt = -1;
+            }
+            Boolean isSuccess = PolicyRuleDAO.deletePolicyRule(idToInt);
+            return ok(isSuccess.toString());
+        }, HttpExecution.fromThread((java.util.concurrent.Executor) ec));
+    }
+
+    public Result clearPolicyRuleForm() {
+        this.updatingId = -1;
+        return ok();
+    }
+}
