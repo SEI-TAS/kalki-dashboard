6395685b9648a62bc64d6db6eb3c33740ddfbff3
Jun <TheJYKoder@gmail.com>
2020-07-02 15:37:13 -0400

    Fixed bug 430

--- dashboard/public/javascripts/dbManagement/deviceType.js
+++ dashboard/public/javascripts/dbManagement/deviceType.js
@@ -54,9 +54,11 @@ jQuery(document).ready(($) => {
             $("#deviceTypeContent .form-group #name").val("");
         });
     });
-
+    console.log("INITIAL LOAD");
+    getDeviceTypes();
     //only load data when tab is active
     $('a[href="#DeviceTypeContent"]').on('shown.bs.tab', function (e) {
+        console.log("SECONDARY LOAD");
         getDeviceTypes();
     });
 });
