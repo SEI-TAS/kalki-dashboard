6835598ed78be2d7f30a2a618b1a87ee86f03967
Jun <TheJYKoder@gmail.com>
2020-08-28 02:00:24 -0400

    Redesigned the Policy Rule View

--- dashboard/public/javascripts/dbManagement/policyRuleView.js
+++ dashboard/public/javascripts/dbManagement/policyRuleView.js
@@ -41,6 +41,25 @@ jQuery(document).ready(($) => {
     let stateTransitionStartToIdMap = {};
     let stateTransitionFinishToIdMap = {};
 
+    let policyConditionIdToAlertTypeIdsMap = {};
+    let policyConditionIdToThresholdMap = {};
+
+    let alertTypeIdToNameMap = {};
+    let alertTypeNameToIdMap = {};
+    let alertTypeIdToDescription = {};
+
+    let stateNameToUmboxes = {};
+
+    let umboxImageIDtoNameMap = {};
+    let umboxImageNameToIdMap = {};
+
+    let commandLookupIdToPolicyRuleId = {};
+    let commandLookupIdToCommandId = {};
+
+    let commandIdToNameMap = {};
+    let commandIdToDeviceTypeIdMap = {};
+    let commandNameToIdMap = {};
+
     /**
      * Generates a legend to help with determining what colors belong to what device types when using the directionalGraph
      * tool.
@@ -75,157 +94,92 @@ jQuery(document).ready(($) => {
         nodeColorsDiv.html(html)
     }
 
-    /**
-     * Create a directional graph based on input data. This graph is color coded for up to 10 unique device types, and
-     * allows the user to move the graph elements around.
-     *
-     * @param links input data used to generate the graph. It needs to be in this format:
-     * {
-     *  "source":"<device type>|<state transition 1>",
-     *  "target":"<device type>|<state transition 2>",
-     *  "deviceType":"<device type>"
-     * }
-     * @param deviceColors A mapping between unique device types and their colors
-     * @param deviceTypes A map of the unique device types being used
-     * @param nodeColors A mapping between unique nodes and their colors
-     */
-    function directionalGraph(links, deviceColors, deviceTypes, nodeColors) {
-        // Get all unique nodes
-        let data = ({nodes: Array.from(new Set(links.flatMap(l => [l.source, l.target])), id => ({id})), links})
+    function drawGraph(links, deviceColors, deviceTypes, nodeColors) {
 
-        // Set the internal scale of the display area. Change these to mess with the scale of the svg
-        let height = 400
-        let width = 700
+    	let data = ({nodes: Array.from(new Set(links.flatMap(l => [l.source, l.target])), id => ({id})), links})
+    	var cy = cytoscape({
+  		  container: document.getElementById('cy')
+  		});
 
-        // Arc function for the link between two nodes
-        function linkArc(d) {
-            const r = Math.hypot(d.target.x - d.source.x, d.target.y - d.source.y);
-            return `
-            M${d.source.x},${d.source.y}
-            A${r},${r} 0 0,1 ${d.target.x},${d.target.y}
-          `;
+  		for(var i = 0; i < data.nodes.length; i++) {
+  			nodeId = data.nodes[i].id;
+  			cy.add({
+  		        data: { id: nodeId, 
+                      name: nodeId},
+  		        }
+  		    );
+
+  		    cy.nodes('[id = \"' + nodeId +'\"]').style('background-color', nodeColors(nodeId));
+  		}
+
+  		for(var i = 0; i < links.length; i++) {
+
+  			cy.add({data: { id: 'trans'+i, 
+  							source: links[i].source, 
+  							target: links[i].target,
+  							policyConditionsAndRatesAndCommands: links[i].policyConditionsAndRatesAndCommands}});
+  		}
+
+  		cy.layout({ name: 'circle'}).run();
+  		cy.zoomingEnabled( false );
+  		cy.nodes().ungrabify();
+  		cy.center();
+
+  		cy.edges('edge').style({
+		    "curve-style": "bezier",
+		    "target-arrow-shape": "triangle"
+		  })
+
+  		cy.on('tap', 'node', function(evt){
+  		  var node = evt.target;
+        var htmlString = "";
+        var umboxes = stateNameToUmboxes[node.data("name")];
+        console.log(links)
+
+
+        htmlString += "Name: " + node.data("name")  + "<br>";
+        htmlString += "Umboxes: ";
+        for(var i = 0; i < umboxes.length-1; i++) {
+          htmlString += umboxes[i] + ", ";
         }
+        htmlString += umboxes[umboxes.length-1];
+  		  $("#selectedElement").html(htmlString);
+  		});
 
-        // Simulation for moving the nodes around
-        drag = simulation => {
-            function dragstarted(d) {
-                if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-                d.fx = d.x;
-                d.fy = d.y;
-            }
-
-            function dragged(d) {
-                d.fx = d3.event.x;
-                d.fy = d3.event.y;
-            }
-
-            function dragended(d) {
-                if (!d3.event.active) simulation.alphaTarget(0);
-                d.fx = null;
-                d.fy = null;
-            }
-
-            return d3.drag()
-                .on("start", dragstarted)
-                .on("drag", dragged)
-                .on("end", dragended);
+  		cy.on('tap', 'edge', function(evt){
+  		  var edge = evt.target;
+  		  var htmlString = "";
+        var listOfPolicyConditions = edge.data("policyConditionsAndRatesAndCommands");
+        for(var i = 0; i < listOfPolicyConditions.length; i++) {
+          var content = "<strong>Sampling Rate</strong>: " + listOfPolicyConditions[i][1] + "<br>";
+          content += "<strong>Commands</strong>: " + listOfPolicyConditions[i][2] + "<br>";
+          var row = '<button class="accordion">'+listOfPolicyConditions[i][0]+
+                '</button><div class="panel"><p>'+ content +'</p></div>'
+          htmlString += row;
         }
-
-        // A function to generate the actual chart (stored in a chart variable)
-        chart = function () {
-            const links = data.links.map(d => Object.create(d));
-            const nodes = data.nodes.map(d => Object.create(d));
-
-            const simulation = d3.forceSimulation(nodes)
-                .force("link", d3.forceLink(links).id(d => d.id))
-                .force("charge", d3.forceManyBody().strength(-400))
-                .force("x", d3.forceX())
-                .force("y", d3.forceY());
-
-            // Attach it to the html item with the id="svg"
-            const svg = d3.selectAll("#svg").append("svg")
-                .attr("viewBox", [-width / 2, -height / 2, width, height])
-                .style("font", "12px sans-serif");
-
-            var div = d3.select("#svg").append("div")	
-			    .attr("class", "tooltip")				
-			    
-
-            // Per-type markers, as they don't inherit styles.
-            svg.append("defs").selectAll("marker")
-                .data(deviceTypes)
-                .join("marker")
-                .attr("id", d => `arrow-${d}`)
-                .attr("viewBox", "0 -5 10 10")
-                .attr("refX", 15)
-                .attr("refY", -0.5)
-                .attr("markerWidth", 6)
-                .attr("markerHeight", 6)
-                .attr("orient", "auto")
-                .append("path")
-                .attr("fill", deviceColors)
-                .attr("d", "M0,-5L10,0L0,5");
-
-            // Create the links between all of the nodes, and color it based on the device type
-            const link = svg.append("g")
-                .attr("fill", "none")
-                .attr("stroke-width", 1.5)
-                .selectAll("path")
-                .data(links)
-                .join("path")
-                .attr("stroke", d => deviceColors(d.deviceType))
-                .attr("marker-end", d => `url(${new URL(`#arrow-${d.deviceType}`, location)})`)
-
-            // For getting text on links, see the second part of this answer:
-            //  https://stackoverflow.com/questions/33165265/show-tool-tip-on-links-of-force-directed-graph-in-d3js
-
-            // Create the nodes
-            const node = svg.append("g")
-                .attr("stroke-linecap", "round")
-                .attr("stroke-linejoin", "round")
-                .selectAll("g")
-                .data(nodes)
-                .join("g")
-                // Change this to change the color of the nodes, based on the name associated with the node
-                .attr("fill", d => {
-                    // Split on the node name, to exclude device type header
-                    var nodeSplit = d.id.split('|')
-                    // Safely get the second element (avoid index out of bounds errors)
-                    nodeSplit.length === 2 ? nodeSplit = nodeSplit[1] : nodeSplit = nodeSplit;
-                    return nodeColors(nodeSplit)
-                })
-                .call(drag(simulation));
-
-            node.append("circle")
-                .attr("stroke", "white")
-                .attr("stroke-width", 1.5)
-                .attr("r", 4)
-
-            node.append("text")
-                .style("fill", "black")  // Set the text to be black
-                .attr("x", 8)
-                .attr("y", "0.31em")
-                // .text(d => {
-                //     var nodeSplit = d.id.split('|')
-                //     nodeSplit.length === 2 ? nodeSplit = nodeSplit[1] : nodeSplit = nodeSplit;
-                //     return nodeSplit
-                // })
-                .clone(true).lower()
-                .attr("fill", "none")
-                .attr("stroke", "white")
-                .attr("stroke-width", 3);
-
-            simulation.on("tick", () => {
-                link.attr("d", linkArc);
-                node.attr("transform", d => `translate(${d.x},${d.y})`);
-            });
-
-            return svg.node();
+        $("#selectedElement").html(htmlString);
+        var acc = document.getElementsByClassName("accordion");
+        for (var i = 0; i < acc.length; i++) {
+          acc[i].addEventListener("click", function() {
+            this.classList.toggle("activeRow");
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+              panel.style.display = "none";
+            } else {
+              panel.style.display = "block";
+            }
+          });
         }
+  		});
 
-        $("#svg").empty()
-        // Make the call to actually draw the chart
-        chart()
+
+
+      cy.on('tap', function(event){
+        var evtTarget = event.target;
+        if( evtTarget === cy ){
+          $("#selectedElement").html("");
+        }
+      });
     }
 
     /**
@@ -275,6 +229,73 @@ jQuery(document).ready(($) => {
         });
     }
 
+
+    async function getPolicyConditions() {
+        return $.get("/policy-conditions", (policyConditions) => {
+            $.each(JSON.parse(policyConditions), (id, policyCondition) => {
+                policyConditionIdToAlertTypeIdsMap[policyCondition.id] = policyCondition.alertTypeIds;
+                policyConditionIdToThresholdMap[policyCondition.id] = policyCondition.threshold;
+            });
+        });
+    }
+
+    async function getAlertTypes() {
+        $("#policyRuleContent #policyRuleAlertTypeSelect").empty();
+
+        return $.get("/alert-types", (alertTypes) => {
+            $.each(JSON.parse(alertTypes), (id, alertType) => {
+                alertTypeIdToNameMap[alertType.id] = alertType.name;
+                alertTypeNameToIdMap[alertType.name] = alertType.id;
+                alertTypeIdToDescription[alertType.id] = alertType.description;
+            });
+        });
+    }
+
+    async function getUmboxImages() {
+        return $.get("/umbox-images", (umboxImages) => {
+            $.each(JSON.parse(umboxImages), (id, umboxImage) => {
+                umboxImageIDtoNameMap[umboxImage.id] = umboxImage.name;
+                umboxImageNameToIdMap[umboxImage.name] = umboxImage.id;
+            });
+        });
+    }
+
+    async function getUmboxLookups() {
+        await getSecurityStates();
+        await getUmboxImages();
+
+        $.get("/get-umbox-lookups-by-device-type?id="+$("#type").val(), (umboxLookups) => {
+            $.each(JSON.parse(umboxLookups), (index, umboxLookup) => {
+                if(stateIdToNameMap[umboxLookup.securityStateId] in stateNameToUmboxes) {
+                  stateNameToUmboxes[stateIdToNameMap[umboxLookup.securityStateId]].push(umboxImageIDtoNameMap[umboxLookup.umboxImageId]) 
+                }
+                else {
+                  stateNameToUmboxes[stateIdToNameMap[umboxLookup.securityStateId]] = [umboxImageIDtoNameMap[umboxLookup.umboxImageId]]
+                }
+            });
+        });
+    }
+
+    async function getCommands() {
+        return $.get("/commands", (commands) => {
+            $.each(JSON.parse(commands), (id, command) => {
+                commandIdToNameMap[command.id] = command.name;
+                commandIdToDeviceTypeIdMap[command.id] = command.deviceTypeId;
+                commandNameToIdMap[command.name] = command.id;
+            });
+        });
+    }
+
+
+    async function getCommandLookups() {
+        return $.get("/command-lookups", (commands) => {
+            $.each(JSON.parse(commands), (id, commandLookup) => {
+                commandLookupIdToPolicyRuleId[commandLookup.id] = commandLookup.policyRuleId
+                commandLookupIdToCommandId[commandLookup.id] = commandLookup.commandId
+            });
+        });
+    }
+
     /**
      * Get the policy rules, then populate the links object with the data necessary to create a chart of device
      * connections over state transitions.
@@ -282,29 +303,76 @@ jQuery(document).ready(($) => {
      * @returns {Promise<*>} Return nothing when done
      */
     async function getPolicyRuleData() {
+        
+        await getCommandLookups();
+        await getCommands();
         links = []
+
+        var stateToLinksDict = {}
         return $.get("/policy-rules-by-id?id="+$("#type").val(), (policyRules) => {
             $.each(JSON.parse(policyRules), (index, policyRule) => {
                 // Get the string names using the provided maps
                 let deviceTypeName;
+                let alertTypeArray = [];
+                policyConditionIdToAlertTypeIdsMap[policyRule.policyConditionId].forEach(element => alertTypeArray.push(alertTypeIdToNameMap[element]));
+
+                /*
                 if(policyRule.deviceTypeId) {
                     deviceTypeName = deviceTypeIdToNameMap[policyRule.deviceTypeId];
                 }
                 else {
-                    deviceTypeName = "All";
+                    deviceTypeName = deviceTypeIdToNameMap[$("#type").val()];
                 }
+                */
+                
+                let deviceCommandArray = [];
+                $.each(commandLookupIdToPolicyRuleId, function (index, policyRuleId) {
+                    if (policyRuleId == policyRule.id) {
+                        deviceCommandArray.push(
+                            deviceTypeIdToNameMap[commandIdToDeviceTypeIdMap[commandLookupIdToCommandId[index]]]
+                            + " - " +
+                            commandIdToNameMap[commandLookupIdToCommandId[index]]
+                        )
+                    }
+                });
+
+
                 let stateSource = stateIdToNameMap[stateTransitionIdToStartMap[policyRule.stateTransitionId]];
                 let stateTarget = stateIdToNameMap[stateTransitionIdToFinishMap[policyRule.stateTransitionId]];
+                let policyConditionName = alertTypeArray.join(" && ");
+                console.log(policyRule)
 
                 // Add the links to the object using the special format
                 // Have to be a little tricky here, and put the device name in front of the state. This lets us
                 //  separate the graphs from one another (x|Normal is different from y|Normal). To get the color to
                 //  work properly, we just remove this leading device name and separator when applying colors in chart
-                links.push({"source": deviceTypeName + "|" + stateSource,
-                    "target": deviceTypeName + "|" + stateTarget,
-                    "deviceType": deviceTypeName
-                });
+                var key = stateSource+"|"+stateTarget;
+                if(key in stateToLinksDict) {
+                  stateToLinksDict[key].push([policyConditionName, policyRule.samplingRateFactor, deviceCommandArray.join(", ")])
+                }
+                else {
+                  stateToLinksDict[key] = [[policyConditionName, policyRule.samplingRateFactor, deviceCommandArray.join(", ")]]
+                }
+                /*
+                links.push({"source": stateSource,
+                    "target": stateTarget,
+                    "deviceType": deviceTypeName,
+                    "policyCondition" : policyConditionName,
+                    "samplingRate" : policyRule.samplingRateFactor,
+                });*/
             });
+            for(var key in stateToLinksDict) {
+              var item = stateToLinksDict[key];
+              var stateSource = key.split("|")[0];
+              var stateTarget = key.split("|")[1];
+              var deviceTypeName = deviceTypeIdToNameMap[$("#type").val()];
+
+              links.push({"source": stateSource,
+                    "target": stateTarget,
+                    "deviceType": deviceTypeName,
+                    "policyConditionsAndRatesAndCommands" : item,
+                });
+            }
         });
     }
 
@@ -317,6 +385,9 @@ jQuery(document).ready(($) => {
         // Get the data needed to generate this graph
         await getDeviceTypes();
         await getStateTransitions();
+        await getAlertTypes();
+        await getPolicyConditions();
+        await getUmboxLookups();
         await getPolicyRuleData();
 
         // Get all individual deviceTypes, so we can group them together
@@ -328,7 +399,7 @@ jQuery(document).ready(($) => {
             ["#4daf4a","#dec100","#e41a1c","#999999","#377eb8","#984ea3","#ff7f00","#a65628","#f781bf"]);
 
         // Make the graph and the legend
-        directionalGraph(links, deviceColors, deviceTypes, nodeColors);
+        drawGraph(links, deviceColors, deviceTypes, nodeColors);
         legend(links, deviceColors, deviceTypes, nodeColors);
     }
 
@@ -340,5 +411,8 @@ jQuery(document).ready(($) => {
     });
     $("#type").change(function() {
         getPolicyRuleView();
+        $("#selectedElement").html("");
+        stateNameToUmboxes = {};
     });
+
 });
