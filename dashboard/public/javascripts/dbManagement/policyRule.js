67a6cec9cda3fbabd449125e0cf85af55d485323
Jun <TheJYKoder@gmail.com>
2020-08-31 20:40:23 -0400

    Fixed bug when removing all commands/alerts after adding

--- dashboard/public/javascripts/dbManagement/policyRule.js
+++ dashboard/public/javascripts/dbManagement/policyRule.js
@@ -89,6 +89,9 @@ jQuery(document).ready(($) => {
 
     let currentUmboxImageIdDagOrderMap = {};
 
+    let totalAlertsAdded = 0;
+    let totalCommandsAdded = 0;
+
     function addAlertTypeRow(alertTypeLookupId) {
         let alertTypeId = alertTypeLookupIdToAlertTypeIdMap[alertTypeLookupId];
 
@@ -113,7 +116,7 @@ jQuery(document).ready(($) => {
             return false;
         } else {
             // No duplicates, so add this row to the form
-            $("#defaultAlertRow").remove();
+            totalAlertsAdded++;
             let currentCount = ++alertTypeRowCounter;
             let newRow = "<tr id='alertTypeOrderTableRow" + currentCount + "'>\n" +
                 "    <td class='fit'><button type='button' class='btn btn-primary btn-sm' id='removeButton" + currentCount + "'>Remove</button></td>" +
@@ -132,10 +135,18 @@ jQuery(document).ready(($) => {
                 $("#alertTypeOrderTableBody #alertTypeOrderTableRow" + currentCount).remove();
 
                 $("#alertTypeOrderFormInput" + alertTypeId).remove();
+                totalAlertsAdded--;
+
+                if(totalAlertsAdded === 0) {
+                    $("#defaultAlertRow").attr("hidden",false);
+                }
 
                 let indexToRemove = currentAlertTypeIds.indexOf(alertTypeId);
                 currentAlertTypeIds.splice(indexToRemove, 1);
             });
+        }
+        if(totalAlertsAdded > 0) {
+            $("#defaultAlertRow").attr("hidden",true);
         }
         return true;
     }
@@ -162,7 +173,7 @@ jQuery(document).ready(($) => {
             return false;
         } else {
             // No duplicates, so add this row to the form
-            $("#defaultCommandRow").remove();
+            totalCommandsAdded++;
             let currentCount = ++commandRowCounter;
             let newRow = "<tr id='policyRuleCommandsOrderTableRow" + currentCount + "'>\n" +
                 "    <td class='fit'><button type='button' class='btn btn-primary btn-sm' id='removeButton" + currentCount + "'>Remove</button></td>" +
@@ -185,9 +196,16 @@ jQuery(document).ready(($) => {
                 $("#policyRuleCommandsOrderTableBody #policyRuleCommandsOrderTableRow" + currentCount).remove();
 
                 $("#deviceCommandFormInput" + currentCount).remove();
+                totalCommandsAdded--;
 
+                if(totalCommandsAdded === 0) {
+                    $("#defaultCommandRow").attr("hidden",false);
+                }
                 delete currentCommandIdToOrderNumberMap[commandId.toString()];
             });
+        }
+        if(totalCommandsAdded > 0) {
+            $("#defaultCommandRow").attr("hidden",true);
         }
         return true;
     }
