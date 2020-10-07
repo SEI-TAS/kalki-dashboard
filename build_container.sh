8ecba8dea03580b6c1564ead63fb0727b1b11bb7
Sebastian Echeverria <secheverria@sei.cmu.edu>
2020-10-07 15:16:57 -0400

    Updating version to 1.8.0

--- build_container.sh
+++ build_container.sh
@@ -18,5 +18,5 @@ fi
 echo "Copying configurations for deployment:"
 cp -r -v $deployment_path/config.json temp.json
 
-KALKI_DB_VER="1.7.0"
+KALKI_DB_VER="1.8.0"
 docker build --network=host --build-arg KALKI_DB_VER="${KALKI_DB_VER}" -t kalki/kalki-dashboard .
